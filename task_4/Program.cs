d6026ae70c2e40a266a7656b06c4674e31c7ee98
Mess <alla-fedia@mail.ru>
2023-03-07 19:52:49 +0300

    четное нечетное

---
+++ task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// является ли число четным
+
+Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = num % 2;
+if (res == 0)
+{
+    Console.WriteLine("четное");
+}
+else
+    Console.WriteLine("нечетное");
