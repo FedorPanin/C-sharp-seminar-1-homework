30e21ac7ba996ab8f8ddba5ae5ea8c1ba3fa87c1
Mess <alla-fedia@mail.ru>
2023-03-07 20:04:04 +0300

    3 число слева

---
+++ task_6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход целое число любой разрядности и на выходе показывает третью цифру слева этого числа или говорит, что такой цифры нет. Через строку решать нельзя.
+Console.WriteLine("введите любое число больше 3х знаков");
+int num = Convert.ToInt32(Console.ReadLine());
+int ostatok = 0;
+if (num < 100)
+{
+    Console.WriteLine("в вашем числе меньше 3х знаков");
+}
+else
+    if (num <= 999)
+    {
+    ostatok = num % 10;  
+    }
+    else
+        while (num > 999)
+        {
+            num = (num / 10);
+            ostatok = num % 10;
+        }
+Console.WriteLine(ostatok);
