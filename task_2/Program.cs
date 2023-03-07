017a0f1f4538c7127ad952a311d7498debe6eb75
Mess <alla-fedia@mail.ru>
2023-03-07 19:34:53 +0300

    макс из 2 чисел

---
+++ task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программму которая на вход принимает 2 числа и выдает, какое число большее, а какое меньшее.
+// a = 5; b = 7 max = 7
+
+Console.WriteLine("введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num1 > num2)
+{
+    Console.WriteLine("максимальное число ");
+    Console.WriteLine(max);
+}
+else
+    max = num2;
+    Console.WriteLine("максимальное число ");
+    Console.WriteLine(max);
