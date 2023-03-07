57e92634afca21d3a2bc17fa5a8c44980461fd0c
Mess <alla-fedia@mail.ru>
2023-03-07 20:01:28 +0300

    целые числа до N

---
+++ task_5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int min = 2;
+while (min <= num)
+{
+    Console.Write(min + " ");
+    min = min + 2;
+}
